ef107948ad1f23c2ea5c85db269fd87fbfa97083
Ivan <ivan1501@gmail.com>
2020-05-11 09:31:00 +0200

    Footer created.

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&#169; 2020 Ivan Radunković - source code on <a href="https://github.com/ivanradunkovic/React-Sudoku">Github</a></p>
+    </footer>
+  )
+}
