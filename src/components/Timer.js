0f334ce959f1b98f8cf362196c547fe4500dfe8b
Ivan <ivan1501@gmail.com>
2020-05-15 19:45:30 +0200

    Timer component completed.

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { useSudokuContext } from '../context/SudokuContext';
 import moment from 'moment';
@@ -15,4 +14,26 @@ export const Timer = (props) => {
   function tick() {
     setCurrentTime(moment());
   }
+
+  function getTimer() {
+    let secondsTotal = currentTime.diff(timeGameStarted, 'seconds');
+    if (secondsTotal <= 0)
+      return '00:00';
+    let duration = moment.duration(secondsTotal, 'seconds');
+    let hours = duration.hours();
+    let minutes = duration.minutes();
+    let seconds = duration.seconds();
+    let stringTimer = '';
+
+    stringTimer += hours ? '' + hours + ':' : '';
+    stringTimer += minutes ? (minutes < 10 ? '0' : '') + minutes + ':' : '00:';
+    stringTimer += seconds < 10 ? '0' + seconds : seconds;
+
+    return stringTimer;
+  }
+
+  return (
+    <div className="status__time">{getTimer()}
+    </div>
+  )
 }
