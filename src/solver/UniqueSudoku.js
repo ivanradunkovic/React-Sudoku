d86f95e09cd1ccc9227058f387fca3dc4d4a2914
Ivan <ivan1501@gmail.com>
2020-05-25 11:57:11 +0200

    Unique Sudoku component completed.

--- src/solver/UniqueSudoku.js
+++ src/solver/UniqueSudoku.js
@@ -53,3 +53,112 @@ function _getIndexOfCell(box, cell) {
     return row * 9 + column;
   }
   
+function _cellAvailable(tempInitArray, box, value) {
+  return tempInitArray[_getIndexOfCell(box, value)] === '0' ? 0 : 1;
+}
+
+/**
+ * Generates a Unique Sudoku puzzle from a solved Sudoku.
+ */
+function _generateUniqueSudoku(solvedArray, difficulty, e) {
+  let currentDifficulty = difficulty;
+  let minimumCells, maximumCells, totalCells, box, cell;
+
+  let tempInitArray = nullArray.slice();
+  let boxCounts = [ 0,0,0,
+                    0,0,0,
+                    0,0,0 ];
+  let boxesAvailable = [];
+  let cellsAvailable = [];
+
+  if (e)
+    currentDifficulty = e.target.value;
+
+  if (currentDifficulty === 'Easy') {
+    minimumCells = 3;
+    maximumCells = 7;
+    totalCells = 45;
+  }
+  else if (currentDifficulty === 'Medium') {
+    minimumCells = 2;
+    maximumCells = 6;
+    totalCells = 40;
+  }
+  else {
+    minimumCells = 1;
+    maximumCells = 5;
+    totalCells = 30;
+  }
+
+  for (let j = 0; j < 9; j++) {
+    boxCounts[j] =  _cellAvailable(tempInitArray, j, 0) +
+                    _cellAvailable(tempInitArray, j, 1) +
+                    _cellAvailable(tempInitArray, j, 2) +
+                    _cellAvailable(tempInitArray, j, 3) +
+                    _cellAvailable(tempInitArray, j, 4) +
+                    _cellAvailable(tempInitArray, j, 5) +
+                    _cellAvailable(tempInitArray, j, 6) +
+                    _cellAvailable(tempInitArray, j, 7) +
+                    _cellAvailable(tempInitArray, j, 8);
+  }
+
+  for (let i = 0; i < totalCells; i++) {
+    boxesAvailable = [];
+    for (let j = 0; j < 9; j++) {
+      if (boxCounts[j] < minimumCells) {
+        boxesAvailable.push(j);
+      }
+    }
+    if (boxesAvailable) {
+      for (let j = 0; j < 9; j++) {
+        if (boxCounts[j] < maximumCells) {
+          boxesAvailable.push(j);
+        }
+      }
+    }
+    box = boxesAvailable[Math.random() * boxesAvailable.length | 0];
+
+    cellsAvailable = [];
+    for (let j = 0; j < 9; j++) {
+      if ( tempInitArray[_getIndexOfCell(box, j)] === '0') {
+        cellsAvailable.push(j);
+      }
+    }
+    cell = cellsAvailable[Math.random() * cellsAvailable.length | 0];
+
+    let index = _getIndexOfCell(box, cell);
+    tempInitArray[index] = solvedArray[index]
+    boxCounts[box]++;
+  }
+
+  return tempInitArray;
+}
+
+export const getUniqueSudoku = (difficulty, e) => {
+  let temporaryInitArray = nullArray.slice();
+  let temporarySolvedArray = nullArray.slice();
+  let sudoku = getSudoku();
+
+  /**
+   * Get Sudoku from sudoku.js
+   */
+  let str = sudoku.generate(60);
+
+  [...str].forEach((value, index) => {
+    temporaryInitArray[index] = value === '.'
+                        ? '0'
+                        : value;
+  });
+
+  /**
+   * Get the solution from sudoku.js
+   */
+  str = sudoku.solve(str);
+  [...str].forEach((value, index) => {
+    temporarySolvedArray[index] = value;
+  });
+
+  temporaryInitArray = _generateUniqueSudoku(temporarySolvedArray, difficulty, e);
+
+  return [temporaryInitArray, temporarySolvedArray];
+}
